5615d38f9b31b24a12a11ca4872e43507dffb57c
Otei <wyz429429@gmail.com>
2018-11-24 21:23:05 +0800

    从数据库里面取出班级学生，渲染后展现浏览器。

--- projec3/booktest/urls.py
+++ projec3/booktest/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^\d+$', views.number),
-    url(r'^How was rong$', views.rong)
+    url(r'^How was rong$', views.rong),
+    url(r'^grades/$', views.grades),
+    url(r'^students/$', views.students),
 ]
