de2c19b1c4ef04d6810e48e959c13b45db36d1b1
Otei <wyz429429@gmail.com>
2018-11-26 21:10:14 +0800

    班级学生关连

--- projec3/booktest/urls.py
+++ projec3/booktest/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^\d+$', views.number),
-    url(r'^How was rong$', views.rong)
+    url(r'^How was rong$', views.rong),
+    url(r'^grades/$', views.grades),
+    url(r'^students/$', views.students),
+    url(r'^grades/(\d+)/$', views.gradesStudents)
 ]
--- projec3/booktest/views.py
+++ projec3/booktest/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from . models import Grades, Students
 
 def index(request):
     return HttpResponse('rong is a good man')
@@ -11,3 +12,19 @@ def number(request):
 
 def rong(request):
     return HttpResponse('rong ia a nice man')
+
+def grades(request):
+    # 去模板里取数据
+    gradesList = Grades.objects.all()
+    # 将数据传递给模板，模板再渲染页面，将渲染好的页面返回给浏览器
+    return render(request, 'grades.html', {"grades": gradesList})
+
+
+def students(request):
+    studentsList = Students.objects.all()
+    return render(request, 'students.html', {"students": studentsList})
+
+def gradesStudents(request, num):
+    grade = Grades.objects.get(pk=num)
+    studentsList = grade.students_set.all()
+    return render(request, 'students.html', {"students": studentsList})
