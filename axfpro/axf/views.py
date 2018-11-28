28069d96ce6bd44711c22b2dc7aa11a5b4362286
Otei <wyz429429@gmail.com>
2018-11-28 21:54:10 +0800

    首页全部完成，以及首页需要的数据全部插入数据库

--- axfpro/axf/views.py
+++ axfpro/axf/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Wheel
+from . models import Wheel, Nav, mustbuy, Shop, MainShow
 # Create your views here.
 from django.http import HttpResponse
 
@@ -7,9 +7,27 @@ from django.http import HttpResponse
 
 def home(request):
     wheelsList = Wheel.objects.all()
+    navList = Nav.objects.all()
+    mustbuyList = mustbuy.objects.all()
+
+    shopList = Shop.objects.all()
+    shop1 = shopList[0]
+    shop2 = shopList[1:3]
+    shop3 = shopList[3:7]
+    shop4 = shopList[7:11]
+
+    mainList = MainShow.objects.all()
     data = {
         "title": '主页',
         "wheelsList": wheelsList,
+        "navList": navList,
+        "mustbuyList": mustbuyList,
+        "shop1": shop1,
+        "shop2": shop2,
+        "shop3": shop3,
+        "shop4": shop4,
+        "mainList": mainList,
+
     }
     return render(request, "axf/home.html", data)
 
