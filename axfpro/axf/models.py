692aeb88f77999a062042bf7dc50d6a0343fdf4a
Otei <wyz429429@gmail.com>
2018-11-29 16:03:58 +0800

    数据表全部创建完成，数据全部插入数据库。首页完成。需要开始第二页展示商品页面

--- axfpro/axf/models.py
+++ axfpro/axf/models.py
@@ -53,3 +53,43 @@ class MainShow(models.Model):
     longname3 = models.CharField(max_length=50)
     price3 = models.CharField(max_length=10)
     marketprice3 = models.CharField(max_length=10)
+
+
+# 分类模型
+class FoodTypes(models.Model):
+    typeid = models.CharField(max_length=10)
+    typename = models.CharField(max_length=20)
+    typesort = models.IntegerField()
+    childtypenames = models.CharField(max_length=150)
+# 商品模型类
+class Goods(models.Model):
+    # 商品id
+    productid = models.CharField(max_length=10)
+    # 商品图片
+    productimg = models.CharField(max_length=150)
+    # 商品名称
+    productname = models.CharField(max_length=50)
+    # 商品长名称
+    productlongname = models.CharField(max_length=100)
+    # 是否精选
+    isxf = models.NullBooleanField(default=False)
+    # 是否买一赠一
+    pmdesc = models.CharField(max_length=10)
+    # 规格
+    specifics = models.CharField(max_length=20)
+    # 价格
+    price = models.CharField(max_length=10)
+    # 超市价格
+    marketprice = models.CharField(max_length=10)
+    # 组id
+    categoryid = models.CharField(max_length=10)
+    # 子类组id
+    childcid = models.CharField(max_length=10)
+    # 子类组名称
+    childcidname = models.CharField(max_length=20)
+    # 详情页id
+    dealerid = models.CharField(max_length=10)
+    # 库存
+    storenums = models.IntegerField()
+    # 销量
+    productnum = models.IntegerField()
